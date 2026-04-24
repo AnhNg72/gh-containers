4ae0b60d4bd18156e07bd45f81e67d609c2db6dc
Anh Ng <VanAnh.Nguyen@liebherr.com>
2026-04-24 15:15:40 +0200

    updated deploy WF

--- data/database.js
+++ data/database.js
@@ -6,7 +6,8 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+//const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/gha-demo?authSource=admin`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
